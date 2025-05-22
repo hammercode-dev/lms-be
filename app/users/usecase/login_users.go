23a1a64fd5bc990c2471c2569df6c420dcdcb428
Farhan <anak.upload90@gmail.com>
2025-05-22 11:55:43 +0800

    fix(usecase): update ForgotPassword and Login methods to handle token generation and error logging correctly

--- app/users/usecase/login_users.go
+++ app/users/usecase/login_users.go
@@ -10,38 +10,42 @@ import (
 
 func (us *usecase) Login(ctx context.Context, userReq domain.Login) (user domain.User, token string, err error) {
 	err = us.dbTX.StartTransaction(ctx, func(txCtx context.Context) error {
-		if err = us.userRepo.CleanupLogoutToken(ctx); err != nil {
-			logrus.Error("us.LoginUser: failed to cleanup token", err)
-			return err
-		}
-		
 		user, err = us.userRepo.FindByEmail(ctx, userReq.Email)
 		if err != nil {
 			logrus.Error("us.LoginUser: failed to login", err)
 			return err
 		}
+	
+		if err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userReq.Password)); err != nil {
+			logrus.Error("us.Login: invalid password")
+			return err
+		}
+	
+		
+		tokenPtr, expiredTime, err := us.jwt.GenerateAccessToken(ctx, &user, 60)
+		token = *tokenPtr
+		
+		if err != nil {
+			logrus.Error("us.Login: failed to login. ", err)
+			return err
+		}
+
+		if err = us.userRepo.UnactivateTokenByUser(ctx, user.ID); err != nil {
+			logrus.Error("us.Login: failed to login. ", err)
+			return err
+		}
+		if err = us.userRepo.StoreToken(ctx, token, expiredTime, user.ID); err != nil {
+			logrus.Error("us.Login: failed to login. ", err)
+			return err
+		}
+
 		return nil
 	})
-	if err != nil {
-		logrus.Error("us.Login: failed to login. ", err)
-		return
-	}
-
-	if err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userReq.Password)); err != nil {
-		logrus.Error("us.Login: invalid password")
-		return
-	}
 
 	if err != nil {
 		logrus.Error("us.Login: failed to login. ", err)
 		return
 	}
 
-	signToken, err := us.jwt.GenerateAccessToken(ctx, &user, 60)
-	if err != nil {
-		logrus.Error("us.Login: failed to login. ", err)
-		return
-	}
-
-	return user, *signToken, nil
+	return user, token, nil
 }
