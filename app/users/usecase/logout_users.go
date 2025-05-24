0d36606db426f8f8ccafe5f9ac00deb66e139dba
Farhan <anak.upload90@gmail.com>
2025-05-24 09:35:04 +0800

    fix: improve error handling in user login, logout, and registration usecase

--- app/users/usecase/logout_users.go
+++ app/users/usecase/logout_users.go
@@ -1,12 +1,25 @@
 package usecase
 
-import "context"
+import (
+	"context"
+
+	"github.com/hammer-code/lms-be/utils"
+	"github.com/sirupsen/logrus"
+)
 
 func (us *usecase) Logout(ctx context.Context, token string) error {
 	jwtData, err := us.jwt.VerifyToken(token)
 	if err != nil {
+		logrus.Error("us.LogoutUser: failed to varify token", err)
+		err = utils.NewInternalServerError(err)
 		return err
 	}
 
-	return us.userRepo.LogoutUser(ctx, token, jwtData.ExpiresAt.Time)
+	err = us.userRepo.LogoutUser(ctx, token, jwtData.ExpiresAt.Time)
+	if err != nil {
+		logrus.Error("us.LogoutUser: failed to logout", err)
+		err = utils.NewInternalServerError(err)
+		return err
+	}
+	return err
 }
--- app/users/usecase/register_users.go
+++ app/users/usecase/register_users.go
@@ -5,16 +5,18 @@ import (
 
 	"github.com/hammer-code/lms-be/constants"
 	"github.com/hammer-code/lms-be/domain"
+	"github.com/hammer-code/lms-be/utils"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func (us *usecase) Register(ctx context.Context, userReq domain.User) (domain.User, error) {
 	user := domain.User{}
-	if err := us.dbTX.StartTransaction(ctx, func(txCtx context.Context) error {
+	err := us.dbTX.StartTransaction(ctx, func(txCtx context.Context) error {
 		hashPassword, err := bcrypt.GenerateFromPassword([]byte(userReq.Password), bcrypt.DefaultCost)
 		if err != nil {
-			logrus.Error("us.Register: failed to register user", err)
+			logrus.Error("us.Register: failed to hash password", err)
+			err = utils.NewInternalServerError(err)
 			return err
 		}
 
@@ -23,14 +25,18 @@ func (us *usecase) Register(ctx context.Context, userReq domain.User) (domain.Us
 		user, err = us.userRepo.CreateUser(ctx, userReq)
 		if err != nil {
 			logrus.Error("us.Register: failed to register users. ", err)
-
+			err = utils.NewInternalServerError(err)
 			return err
 		}
 		return nil
 
-	}); err != nil {
-		logrus.Error("us.Register: failed to get users. ", err)
+	})
+
+	if err != nil {
+		logrus.Error("us.Register: failed to register users. ", err)
+		err = utils.NewInternalServerError(err)
 		return domain.User{}, err
 	}
+	
 	return user, nil
 }
