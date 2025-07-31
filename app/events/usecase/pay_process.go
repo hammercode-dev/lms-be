3ce4d4bfa2e5a720cc0c2416d6dbc4aa29c33431
Kocannn <dwicandraandika4@gmail.com>
2025-07-31 20:48:35 +0800

    feat(email): send registration status emails for events
    Add email notifications for event registration and payment status using
    HTML templates. Introduce new email templates for pending payment and
    successful registration. Refactor event usecase to support sending
    emails
    with dynamic data and templates. Improve error handling and logging for
    email operations.

--- app/events/usecase/pay_process.go
+++ app/events/usecase/pay_process.go
@@ -3,8 +3,13 @@ package usecase
 import (
 	"context"
 	"errors"
+	"fmt"
+	"html/template"
+	"os"
+	"time"
 
 	"github.com/hammer-code/lms-be/domain"
+	"github.com/hammer-code/lms-be/pkg/email"
 	"github.com/sirupsen/logrus"
 )
 
@@ -15,6 +20,7 @@ func (uc usecase) PayProcess(ctx context.Context, payload domain.PayProcessPaylo
 		return err
 	}
 
+	logrus.Info("registration event: ", rEvent)
 	if rEvent.ID == 0 {
 		return errors.New("registration order not found")
 	}
@@ -55,6 +61,79 @@ func (uc usecase) PayProcess(ctx context.Context, payload domain.PayProcessPaylo
 			return err
 		}
 
+		// Get the complete event details
+		event, err := uc.repository.GetEvent(txCtx, rEvent.EventID)
+		if err != nil {
+			logrus.Error("failed to get event details")
+			return err
+		}
+
+		// Format the date in a readable format
+		var formattedDate string
+		if event.Date.Valid {
+			formattedDate = event.Date.Time.Format("Monday, 02 January 2006")
+		} else {
+			formattedDate = "Date to be announced"
+		}
+
+		// Read HTML template for email
+		var htmlTmpl []byte
+		if payload.Status == "SUCCESS" {
+			htmlTmpl, err = os.ReadFile("./assets/event_status_registration_sucess_template.html")
+		} else {
+			htmlTmpl, err = os.ReadFile("./assets/event_status_registration_template.html")
+		}
+		if err != nil {
+			return fmt.Errorf("failed to read file template: %w", err)
+		}
+
+		// Parse the HTML template
+		tmpl, err := template.New("register_event_status").Parse(string(htmlTmpl))
+		if err != nil {
+			return fmt.Errorf("failed to parse template: %w", err)
+		}
+
+		// Prepare data for the email template
+		smtpConfig := email.SMTP{
+			Email:    uc.cfg.SMTP_EMAIL,
+			Password: uc.cfg.SMTP_PASSWORD,
+			Host:     uc.cfg.SMTP_HOST,
+			Port:     uc.cfg.SMTP_PORT,
+		}
+
+		// Prepare the receiver data
+		emailPayload := email.NewSendEmail(
+			ctx,
+			smtpConfig,
+			"MIME-Version: 1.0\r\nContent-Type: text/html; charset=UTF-8",
+			"Update: Your Event Registration Status",
+			tmpl,
+		)
+
+		// Add the receiver's email and data to the payload
+		if err := emailPayload.AddReceiver(
+			ctx,
+			email.Receiver{
+				Email: rEvent.Email,
+				Data: map[string]interface{}{
+					"name":     rEvent.Name,
+					"title":    event.Title,
+					"price":    event.Price,
+					"email":    rEvent.Email,
+					"order_no": rEvent.OrderNo,
+					"status":   payload.Status,
+					"note":     payload.Note,
+					"year":     time.Now().Format("2006"),
+					"date":     formattedDate,
+					"duration": event.Duration,
+					"location": event.Location,
+				},
+			}); err != nil {
+			return fmt.Errorf("failed to add receiver: %w", err)
+		}
+
+		// Send the email
+		emailPayload.SendEmail(ctx)
 		return nil
 	})
 
